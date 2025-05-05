6363ab20b7452bdc1daa69f088df2749e9872322
Yaksh3102 <yaksh.inbweb25@gmail.com>
2025-05-05 10:05:26 +0530

    updated

--- server.js
+++ server.js
@@ -4,16 +4,47 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
-const socketio = require('socket.io');
-const http = require('http');
+const socketio = require("socket.io");
+const http = require("http");
 const path = require("path");
 const { throws } = require("assert");
 require("dotenv").config();
 const axios = require("axios");
-const eventEmitter = require('./eventEmitter');
+const eventEmitter = require("./eventEmitter");
 const jwt = require("jsonwebtoken");
 
+const client = require("prom-client"); // metric collection
+const responseTime = require("response-time");
 
+const { createLogger, transports } = require("winston");
+const LokiTransport = require("winston-loki");
+const { log } = require("console");
+
+// Collect default metrics
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register });
+
+const reqResTime = new client.Histogram({
+  name: "http_express_req_res_time",
+  help: "Duration of HTTP requests in ms",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [1, 50, 100, 200, 300, 400, 500, 800, 1000, 2000],
+});
+
+const totalReqCounter = new client.Counter({
+  name: "total_req",
+  help: "Total Number of  requests",
+});
+
+const options = {
+  transports: [
+    new LokiTransport({
+      labels: { appName: "piztaalian" },
+      host: "http://127.0.0.1:3100",
+    }),
+  ],
+};
+const logger = createLogger(options);
 
 global.__basedir = __dirname;
 const allowedOrigin1 = process.env.REACT_APP_API_URL;
@@ -26,20 +57,20 @@ const io = socketio(server, {
     origin: [allowedOrigin1, allowedOrigin2],
     methods: ["GET", "POST"],
     credentials: true,
-  }
+  },
 });
 
 // Logging connection events for better traceability
 io.on("connection", (socket) => {
-  console.log(`Client connected: ${socket.id}`);
-  
+  logger.info(`Client connected: ${socket.id}`);
+
   socket.on("disconnect", () => {
-    console.log(`Client disconnected: ${socket.id}`);
+    logger.info(`Client disconnected: ${socket.id}`);
   });
 });
 
 io.on("error", (err) => {
-  console.error("Socket.IO error event:", err);
+  logger.error("Socket.IO error event:", err);
 });
 
 // io.use((socket, next) => {
@@ -55,7 +86,6 @@ io.on("error", (err) => {
 //     next();
 //   });
 // });
-
 
 // Subscribe to eventEmitter events from controllers and broadcast them to clients
 eventEmitter.on("newOrder", (order) => {
@@ -91,7 +121,7 @@ eventEmitter.on("ordersUpdateTrigger", () => {
 //   if (!token) {
 //     return next(new Error("Authentication error: Token missing"));
 //   }
-  
+
 //   jwt.verify(token, process.env.ADMIN_JWT_SECRET_TOKEN, (err, decoded) => {
 //     if (err) {
 //       return next(new Error("Authentication error: Invalid token"));
@@ -101,12 +131,14 @@ eventEmitter.on("ordersUpdateTrigger", () => {
 //   });
 // });
 
-
 let databasestatus = "In-Progress";
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Adjust the '*' to your frontend's origin if possible
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*"); // Adjust the '*' to your frontend's origin if possible
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.options("*", cors());
@@ -117,23 +149,41 @@ mongoose
   .connect(process.env.DATABASE, { useNewUrlParser: true })
   .then(() => {
     databasestatus = "DB connected";
-    console.log("DB connected");
+    logger.info("DB connected");
   })
   .catch((err) => {
     databasestatus = err;
-    console.log("DB Error => ", err);
+    logger.error("DB Error => ", err);
   });
 
 // autoIncrement.initialize(mongoose.connection);
 
 //middlewares
 app.use(morgan("dev"));
+// app.use(
+//   morgan("combined", {
+//     stream: { write: (msg) => logger.info(msg.trim()) },
+//   })
+// );
+
+app.use(
+  responseTime((req, res, time) => {
+    totalReqCounter.inc();
+    reqResTime
+      .labels({
+        method: req.method,
+        route: req.url,
+        status_code: res.statusCode,
+      })
+      .observe(time);
+  })
+);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("files"));
 // Serve static files from the 'uploads' directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //routes
 // app.use("/api", authRoutes);
@@ -152,6 +202,13 @@ app.get("/api", (req, res) => {
 
 app.get("/error", (req, res) => {
   res.test("hit the api button. v-24.01.2024.");
+});
+
+// Prometheus metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  const metrics = await client.register.metrics();
+  res.send(metrics);
 });
 
 app.use(async (err, req, res, next) => {
@@ -174,7 +231,7 @@ app.use(async (err, req, res, next) => {
       JSON.stringify(writecontent),
       function (err) {
         if (err) throw err;
-        console.log("Saved!");
+        logger.info("Saved!");
       }
     );
   } catch (error) {}
@@ -189,5 +246,5 @@ app.use(async (err, req, res, next) => {
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
-  console.log(`Server and Socket.IO are running on port ${port}`);
+  logger.info(`Server and Socket.IO are running on port ${port}`);
 });
